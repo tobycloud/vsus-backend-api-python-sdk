f46b4953c260ee113ca3af19313e081330a72733
Toby <newtobycm@gmail.com>
2023-08-26 14:52:57 -0700

    hết lỗi

--- backend_api/__init__.py
+++ backend_api/__init__.py
@@ -2,7 +2,7 @@
 Backend API
 """
 
-from api import API
-from instance import Instance
-from user import User
-from workspace import Workspace
+from .api import API
+from .instance import Instance
+from .user import User
+from .workspace import Workspace
