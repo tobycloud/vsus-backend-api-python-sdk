e9fcbbb23204df9973fe69729ae16a9ddbe2cd45
Toby <newtobycm@gmail.com>
2023-08-29 19:04:26 -0700

    fix: :fire: add pyi files to data_files
    testing on prod xd

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
+import os
+
 from setuptools import find_packages, setup
 
-with open("requirements.txt", "r", encoding="utf8") as file:
-    install_requires = file.read().splitlines()
+with open("requirements.txt", "r", encoding="utf8") as reqs:
+    install_requires = reqs.read().splitlines()
+
+data_files = [
+    "backend_api/" + file for file in os.listdir("backend_api") if file.endswith(".pyi")
+]
 
 setup(
     name="backend_api",
-    version="0.1.6",
+    version="0.1.7",
     packages=find_packages(),
+    data_files=[("backend_api", data_files)],
     install_requires=install_requires,
 )
