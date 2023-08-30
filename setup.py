42255d971f49d92ac099e3bc3c81a50e8ff833f4
Toby <newtobycm@gmail.com>
2023-08-29 19:08:34 -0700

    fix: :ambulance: ok didn't work first try

--- setup.py
+++ setup.py
@@ -1,18 +1,14 @@
-import os
-
 from setuptools import find_packages, setup
 
 with open("requirements.txt", "r", encoding="utf8") as reqs:
     install_requires = reqs.read().splitlines()
 
-data_files = [
-    "backend_api/" + file for file in os.listdir("backend_api") if file.endswith(".pyi")
-]
-
 setup(
     name="backend_api",
     version="0.1.7",
     packages=find_packages(),
-    data_files=[("backend_api", data_files)],
+    package_data={
+        "backend_api": ["*.pyi"],
+    },
     install_requires=install_requires,
 )
