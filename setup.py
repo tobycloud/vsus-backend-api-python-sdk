8560d9bcad45075be82eccd5ee7cdbd2d3fe7390
Toby <newtobycm@gmail.com>
2023-08-29 21:21:25 -0700

    chore: :arrow_up: bump ver

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt", "r", encoding="utf8") as reqs:
 
 setup(
     name="backend_api",
-    version="0.1.8",
+    version="0.1.9",
     packages=find_packages(),
     package_data={
         "backend_api": ["*.pyi"],
